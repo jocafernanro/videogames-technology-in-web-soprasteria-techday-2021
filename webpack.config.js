5132cc227fbdb8c4d3bdd86f044d5f64d4164501
José Carlos Fernández Rodríguez <jcfernandez@Joses-Mac-mini.local>
2021-11-08 22:45:02 +0100

    all changes before scale update

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: "inline-source-map",
   devServer: {
     static: {
-      directory: path.join(__dirname, "public"),
+      directory: path.join(__dirname, "src"),
     },
     compress: true,
     port: 9000,
